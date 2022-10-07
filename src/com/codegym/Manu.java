a98a9e24621b43ba95ee5dddf7f3cabd575b3350
ADMIN <taiphiets666@gmail.com>
2022-10-07 08:41:57 +0700

    daonguocphantu

---
+++ src/com/codegym/Manu.java
@@ -0,0 +1,15 @@
+package com.codegym;
+
+import java.util.Random;
+
+public class Manu {
+    public static void main(String[] args) {
+        int choice = - 1;
+        System.out.println("Menu");
+        System.out.println("1. Draw the triangle");
+        System.out.println("2. Draw the square");
+        System.out.println("3. Draw the rectangle");
+        System.out.println("0. Exit");
+        System.out.println("Enter your choice: ");
+    }
+}
